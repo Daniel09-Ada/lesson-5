14e6589481b5d4a69913479d2054178d88984468
Daniel09_Ada <dadasarov@gmail.com>
2024-09-23 18:37:36 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [users , setUsers] = useState([])
+
+  const promise = () => {
+    fetch("https://jsonplaceholder.typicode.com/photos?_limit=5" , {
+      method : "GET"
+    }).then((response) => {
+      return response.json()
+    }).then((data) => {
+      setUsers(data)
+    })
+
+  }
+
+
+  useEffect(() => {
+    
+    promise()
+
+  }, [] )
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    {users.map((el) => {
+      return(
+        <div>
+
+          <h1 style={{margin: "auto" , marginTop: "20px" , padding: "5px", color: "white", backgroundColor: "black"  , width: "800px" , height: "100px" , border: "2px solid"}}
+          >{el.url}</h1>
+  
+        </div>
+      )
+    })}
+
     </div>
   );
 }
